ccdfcf8d9dee6231cf779f78e127cbe03dc18b5e
Diegod777 <diegoacevedoleandrosilva@gmail.com>
2023-09-02 18:59:04 -0400

    Avance Pasajero

--- src/app/pages/inicio-pasajero/inicio-pasajero.page.ts
+++ src/app/pages/inicio-pasajero/inicio-pasajero.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-pasajero',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPasajeroPage implements OnInit {
 
-  constructor() { }
+  viajes: any = [
+    {
+      id: 1,
+      nombre: "Nombre del viaje",
+      distancia: 13.7,
+      duracion: 18,
+      tarifa: 3000,
+      horaSalida: "18:30"
+    },
+    {
+      id: 2,
+      nombre: "Viaje 2",
+      distancia: 16.3,
+      duracion: 21,
+      tarifa: 4000,
+      horaSalida: "14:00"
+    }
+  ]
+
+  usuario: any = {
+    nombre: "Juan Carlos",
+    apellido: "Bodoque",
+    patente: "TP8373",
+    imagen: "assets/icon/userIcon.jpg"
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  solicitarViaje() {
+    console.log("Solicitar Viaje");
+    // Aquí puedes agregar la lógica para solicitar un viaje
+  }
+
+  getViajeFromId(id: number) {
+    let res: any = null;
+    this.viajes.forEach((v: any) => {
+      if (v.id == id) res = v;
+    })
+    return res;
+  }
+
+  verViaje(id: number) {
+    console.log("Ver Viaje(" + id + ")");
+    let ne: any = {
+      state: {
+        viaje: this.getViajeFromId(id),
+        viewType: "view"
+      }
+    }
+    this.router.navigate(['/ver-viaje'], ne); // Asegúrate de tener una ruta 'ver-viaje'
   }
 
 }
--- src/app/pages/perfil-pasajero/perfil-pasajero.page.ts
+++ src/app/pages/perfil-pasajero/perfil-pasajero.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-perfil-pasajero',
-  templateUrl: './perfil-pasajero.page.html',
-  styleUrls: ['./perfil-pasajero.page.scss'],
+  selector: 'app-perfil-pasajero', // Cambia el selector a 'app-perfil-pasajero'
+  templateUrl: './perfil-pasajero.page.html', // Asegúrate de tener una vista HTML correspondiente
+  styleUrls: ['./perfil-pasajero.page.scss'], // Asegúrate de tener un archivo de estilos correspondiente
 })
 export class PerfilPasajeroPage implements OnInit {
+
+  usuario: any = {
+    nombre: "Nombre del Pasajero",
+    apellido: "Apellido del Pasajero",
+    informacionAdicional: "Información adicional del pasajero", // Agregado un campo adicional
+    imagen: "assets/icon/userIcon.jpg"
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  actualizarDatos() {
+    console.log("Actualizar datos del pasajero");
+    // Implementa la lógica para actualizar los datos del pasajero
+  }
+
+  actualizarContrasena() {
+    console.log("Actualizar contraseña del pasajero");
+    // Implementa la lógica para actualizar la contraseña del pasajero
+  }
+
+  eliminarCuenta() {
+    console.log("Eliminar cuenta del pasajero");
+    // Implementa la lógica para eliminar la cuenta del pasajero
+  }
+
+  editarImagenPerfil() {
+    console.log("Editar imagen de perfil del pasajero");
+    // Implementa la lógica para editar la imagen de perfil del pasajero
+  }
 }
