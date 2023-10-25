7cf7d9a0f4db9b9536e8e91a6a30b950773afbd4
FelipeMunhoz18 <f.munoz7@duocuc.cl>
2023-10-25 15:05:51 -0300

    Sincronizando usuario y conductor
    Sincronizando usuario y conductor en registro y perfil

--- src/app/pages/perfil-pasajero/perfil-pasajero.page.ts
+++ src/app/pages/perfil-pasajero/perfil-pasajero.page.ts
@@ -1,207 +1,177 @@
-import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AlertController, ToastController } from '@ionic/angular';
-import { PhotoService } from 'src/app/services/photo.service';
-//import { DataService } from './data.service'; //NOMBRE DEL SERVICIO
-
+import { ToastController } from '@ionic/angular';
+import { BdserviceService } from 'src/app/services/bdservice.service';
 @Component({
   selector: 'app-perfil-pasajero', 
   templateUrl: './perfil-pasajero.page.html', 
   styleUrls: ['./perfil-pasajero.page.scss'], 
 })
 export class PerfilPasajeroPage implements OnInit { 
-
-  usuario : any={
-    nombre:"Tulio",
-    apellido:"Triviño",
-    imagen:"assets/icon/user_tulio.jpg"
-  }
-
-  newPassA:string="";
-  newPassB:string="";
-  formDatos!: FormGroup;
-  formErrors:any={};
+  usuario: any = {};
+  imagen: string | null = null;
+  contrasenaActual: string = '';
+  nuevaContrasena: string = '';
+  confirmarNuevaContrasena: string = '';
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
     private toastController: ToastController,
-    private alertController: AlertController,
-    private photoService: PhotoService,
-    //private dataService: DataService
-  ) {}
-  ngOnInit() {
-    this.formDatos = this.initForm();
+    private db: BdserviceService
+  ) {
+    this.usuario.imagen = null; // Inicializa la propiedad imagen en null
   }
 
-  initForm():FormGroup{
-    return this.fb.group({
-      nombre: ['', []],
-      apellido: ['', []],
-      patente: ['', []],
+  ngOnInit() {
+    this.db.dbState().subscribe((res) => {
+      if (res) {
+        this.loadUsuario();
+      }
     });
   }
 
-  async showToast(text:string,type:string) {
+  async loadUsuario() {
+    let uID = localStorage.getItem("uID");
+    if (uID) this.usuario = await this.db.leerUsuarioPorID(uID);
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      nombre: [''],
+      correo: [''],
+      numero_cel: [''],
+      telefono: [''],
+      patente: [''],
+    });
+  }
+
+  async showToast(text: string, type: string) {
     const toast = await this.toastController.create({
       message: text,
       duration: 5000,
       position: 'bottom',
       color: type,
-      buttons:[{
-        icon:"close",
-        role:"cancel"
-      }]
+      buttons: [
+        {
+          icon: "close",
+          role: "cancel",
+        },
+      ],
     });
     await toast.present();
   }
 
-  async alertaEliminar() {
-    const alert = await this.alertController.create({
-      header: '¿Desea eliminar esta cuenta?',
-      subHeader:'Esto no se puede deshacer',
-      buttons: [{
-          text:'Cancelar',
-          role:"cancel",
-        },{
-          text:'Confirmar',
-          role:"confirm",
-          cssClass:"color-danger",
-          handler:()=>{
-            this.router.navigate(["/"]);
-          }
-        }
-      ],
-    });
-    /* Se le pueden añadir eventos
-    alert.addEventListener("didDismiss",()=>{
-      this.location.back()
-    })
-    */
-    await alert.present();
-  }
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
-  }
-  
-  actualizarDatos() {
-    this.formErrors = {};
-    let valid: boolean = true;
-    valid = valid && this.validarNombre();
-    valid = valid && this.validarApellido();
-    valid = valid && this.validarPatente();
-    if (valid) {
-      /*this.dataService.actualizarDatos({
-        nombre: this.usuario.nombre,
-        apellido: this.usuario.apellido,
-        patente: this.usuario.patente
-      }).subscribe(
-        (response:any) => {
-          this.showToast('Datos actualizados', 'success');
-        },
-        (error:any) => {
-          // Manejar errores
-        }
-      );*/
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.usuario.imagen = URL.createObjectURL(file); // Almacena la ruta de la imagen en el usuario
     }
   }
 
-  actualizarContrasena() {
-    this.formErrors = {};
-    let valid: boolean = true;
-    valid = valid && this.validarContraA();
-    valid = valid && this.validarContraB();
-    if (valid) {
-      /*this.dataService.actualizarContrasena(this.newPassA).subscribe(
-        (response:any) => {
-          this.showToast('Contraseña actualizada', 'success');
-        },
-        (error:any) => {
-          // Manejar errores
-        }
-      );*/
+  cambiarContrasena() {
+    // Validar que la contraseña actual sea correcta
+    if (!this.validarContrasenaActual()) {
+      this.showToast('La contraseña actual es incorrecta', 'danger');
+      return;
     }
+
+    // Validar que la nueva contraseña y la confirmación coincidan
+    if (this.nuevaContrasena !== this.confirmarNuevaContrasena) {
+      this.showToast('Las contraseñas nuevas no coinciden', 'danger');
+      return;
+    }
+
+    // Validar que la nueva contraseña cumple con tus requisitos de seguridad
+    if (!this.validarNuevaContrasena()) {
+      this.showToast('La nueva contraseña no cumple con los requisitos de seguridad', 'danger');
+      return;
+    }
+
+    // Actualizar la contraseña en la base de datos
+    this.db.actualizarContrasena(this.usuario.id_usuario, this.nuevaContrasena)
+      .then(() => {
+        this.showToast('Contraseña actualizada exitosamente', 'success');
+        // Limpiar campos de contraseña
+        this.contrasenaActual = '';
+        this.nuevaContrasena = '';
+        this.confirmarNuevaContrasena = '';
+      })
+      .catch(error => {
+        this.showToast('Error al actualizar la contraseña: ' + error.message, 'danger');
+      });
   }
 
-  validarNombre(){
-    let valid=true;
-    this.formErrors["nombre_empty"]=false;
-    if(this.usuario.nombre.trim()==""){
-      this.formErrors["nombre_empty"]=true; valid=false;}
-    return valid;
+  async validarContrasenaActual(): Promise<boolean> {
+    const contrasenaAlmacenada = await this.obtenerContrasenaAlmacenada(this.usuario.id_usuario);
+    return this.contrasenaActual === contrasenaAlmacenada;
   }
 
-  validarApellido(){
-    let valid=true;
-    this.formErrors["apellido_empty"]=false;
-    if(this.usuario.apellido.trim()==""){
-      this.formErrors["apellido_empty"]=true; valid=false;}
-    return valid;
-  }
-
-  validarPatente(){
-    let valid=true;
-    this.formErrors["patente_empty"]=false;
-    this.formErrors["patente_invalid"]=false;
-    if(this.usuario.patente.trim()==""){
-      this.formErrors["patente_empty"]=true; valid=false;}
-    if(!this.usuario.patente.match(/[A-Z]{2}[1-9][0-9]{3}|[BCDFGHJKLPRSTVWXYZ]{4}[1-9][0-9]/)){
-      this.formErrors["patente_invalid"]=true; valid=false;}
-    return valid;
-  }
-
-  validarContraA(){
-    let valid=true;
-    this.formErrors["passA_empty"]=false;
-    this.formErrors["passA_short"]=false;
-    this.formErrors["passA_long"]=false;
-    this.formErrors["passA_number"]=false;
-    this.formErrors["passA_upper"]=false;
-    this.formErrors["passA_special"]=false;
-    if(this.newPassA.trim()==""){ 
-      this.formErrors["passA_empty"]=true; valid=false;}
-    if(this.newPassA.trim().length<8){ 
-      this.formErrors["passA_short"]=true; valid=false;}
-    if(this.newPassA.trim().length>40){ 
-      this.formErrors["passA_long"]=true; valid=false;}
-    if(!this.newPassA.match(/[0-9]/)){ 
-      this.formErrors["passA_number"]=true; valid=false;}
-    if(!this.newPassA.match(/[A-Z]/)){ 
-      this.formErrors["passA_upper"]=true; valid=false;}
-    if(!this.newPassA.match(/[!#$%&=+]/)){ 
-      this.formErrors["passA_special"]=true; valid=false;}
-    return valid;
-  }
-
-  validarContraB(){
-    let valid=true;
-    this.formErrors["passB_empty"]=false;
-    this.formErrors["passB_match"]=false;
-    if(this.newPassB.trim()==""){ 
-      this.formErrors["passB_empty"]=true; valid=false;}
-    if(this.newPassA.trim()!=this.newPassB.trim()){ 
-      this.formErrors["passB_match"]=true; valid=false;}
-    return valid;
-  }
-
-  eliminarCuenta() {
-    /*this.dataService.eliminarCuenta().subscribe(
-      () => {
-        this.alertaEliminar();
-        this.router.navigate(["/"]);
-      },
-      (error:any) => {
-        console.error("Error al eliminar la cuenta:", error);
-        this.showToast("Error al eliminar la cuenta", "danger");
+  async obtenerContrasenaAlmacenada(idUsuario: number): Promise<string> {
+    try {
+      const usuario = await this.db.leerUsuarioPorID(idUsuario.toString());
+      if (usuario) {
+        return usuario.password;
       }
-    );*/
+      return '';
+    } catch (error) {
+      // Maneja errores aquí
+      console.error('Error al obtener contraseña almacenada:', error);
+      return '';
+    }
   }
-  
-  editarImagenPerfil(){
-    console.log("!:editarImagenPerfil()");
-    //se abre la cámara o la galería, y permite subir una imagen. si se actualizan los datos,
-    //se reemplaza la imagen en la db
+
+  validarNuevaContrasena(): boolean {
+    // Añade tus criterios de validación aquí
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(this.nuevaContrasena);
+  }
+
+  guardarCambios1() {
+    // Validaciones de contraseña actual y nueva contraseña
+    if (!this.validarContrasenaActual()) {
+      this.showToast("La contraseña actual es incorrecta", "danger");
+      return;
+    }
+
+    if (!this.validarNuevaContrasena()) {
+      this.showToast("La nueva contraseña no cumple con los requisitos de seguridad", "danger");
+      return;
+    }
+
+    // Cambiar la contraseña en la base de datos
+    this.db.actualizarContrasena(this.usuario.id_usuario, this.nuevaContrasena)
+      .then(() => {
+        this.showToast("Contraseña cambiada exitosamente", "success");
+      })
+      .catch((error) => {
+        this.showToast("Error al cambiar la contraseña: " + error.message, "danger");
+      });
+  }
+
+  guardarCambios() {
+    this.db.updateUsuario(this.usuario).then((message) => {
+      // Guarda la imagen en la base de datos
+      if (this.usuario.imagen) {
+        this.db.updateImagenUsuario(this.usuario.id_usuario, this.usuario.imagen)
+          .then(() => {
+            this.showToast("Usuario actualizado exitosamente", "success");
+          })
+          .catch((error) => {
+            this.showToast("Error al guardar la imagen de perfil: " + error.message, "danger");
+          });
+      } else {
+        this.showToast(message, "success");
+      }
+    }).catch((error) => {
+      console.error('Error al insertar datos:', error);
+      let errorMessage = "Error al guardar cambios: ";
+      if (error.message) {
+        errorMessage += error.message;
+      } else {
+        errorMessage += "Detalles específicos del error no disponibles.";
+      }
+      this.showToast(errorMessage, "danger");
+    });
   }
 }
