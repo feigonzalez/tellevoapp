c533cdd2f8ab668bc0ed70a540de7bc4e289abbe
elxumare1 <fei.gonzalezg@duocuc.cl>
2023-10-31 16:39:04 -0300

    Añadidos apuntes

--- src/app/pages/administrar-viaje/administrar-viaje.page.spec.ts
+++ src/app/pages/administrar-viaje/administrar-viaje.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { AdministrarViajePage } from './administrar-viaje.page';
 
 describe('AdministrarViajePage', () => {
--- src/app/pages/bdreport/bdreport.page.spec.ts
+++ src/app/pages/bdreport/bdreport.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { BdreportPage } from './bdreport.page';
 
 describe('BdreportPage', () => {
