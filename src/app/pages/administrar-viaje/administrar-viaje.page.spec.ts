3ca43b85b4c967ad66bd1b899f4dd072ca6865b2
Alv4r0o0o0 <f.munoz7@duocuc.cl>
2023-11-02 17:33:30 -0300

    Añadidas carpetas www y android

--- src/app/pages/administrar-viaje/administrar-viaje.page.spec.ts
+++ src/app/pages/administrar-viaje/administrar-viaje.page.spec.ts
@@ -1,15 +1,15 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed} from '@angular/core/testing';
 import { AdministrarViajePage } from './administrar-viaje.page';
 
 describe('AdministrarViajePage', () => {
   let component: AdministrarViajePage;
   let fixture: ComponentFixture<AdministrarViajePage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
     fixture = TestBed.createComponent(AdministrarViajePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
